6d8b6e2cef52572946f70a419c822dd7313543e6
Marc Wonneberger <marc@wonneberger.xyz>
2022-10-29 21:46:12 +0200

    updated dependencies

--- app/setup.py
+++ app/setup.py
@@ -15,7 +15,7 @@ OPTIONS = {
 
 setup(
     app=APP,
-    name='mtoolbox',
+    name='mtoolbox2',
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'], install_requires=['rumps','yt_dlp', 'clipboard', 'validators']
